cea20190e35945480f0539ad4295cb856c8d45d1
aachalmakode <aachalmakode900@gmail.com>
2021-07-02 00:32:19 -0400

    Simple Link list

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -1,4 +1,46 @@
 package com.ds;
  public class LinkedList {
-}
+  Node head;
 
+  public void insert(int i) {
+  }
+
+  public void print() {
+  }
+
+  class Node {
+   Node next;
+   int data;
+
+   //constructor
+   public Node(int data) {
+    super();
+    this.next = next;
+    this.data = data;
+   }
+
+   public Node insert(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+     head = newNode;
+    } else {
+     Node temp = head;
+     while (temp.next != null) {
+      temp = temp.next;
+     }
+     temp.next = newNode;
+    }
+    return newNode;
+   }
+
+   public void print() {
+    Node temp = head;
+    while (temp != null) {
+     System.out.print(temp.data + " -> ");
+     temp = temp.next;
+    }
+    System.out.println();
+   }
+
+  }
+ }
--- src/com/ds/MainClass.java
+++ src/com/ds/MainClass.java
@@ -1,5 +1,12 @@
 package com.ds;
-        import com.ds.LinkedList;
-public class MainClass {
+import com.ds.LinkedList;
 
+    public class MainClass {
+        public static void main(String[] args) {
+            LinkedList linkList = new LinkedList();
+            linkList.insert(70);
+            linkList.insert(30);
+            linkList.insert(56);
+            linkList.print();
+        }
     }
